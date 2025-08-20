75ac6868d5b6021b50398ba5099cb3828028d8fc
Rashi-Domadiya <rashidomadiya.ce@gmail.com>
2025-08-20 09:16:22 -0700

    Add files via upload

---
+++ Practical 14/Practical14.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int Balance=5000;
+    int withdraw,choice;
+    printf(
+    "Welcome to National Bank Of Bharat ATM\n");
+    printf("Your starting Balance is: %d\n", Balance);
+    while(1)
+    {
+        printf(
+        "\nEnter the amount to withdraw:");
+        scanf("%d",&withdraw);
+
+        if(withdraw<=Balance)
+        {
+            Balance-=withdraw;
+            printf("\nTransaction successful!\n");
+            printf("\nRemaining Balance: %d\n", Balance);
+        }
+        else
+        {
+            printf("insufficient balance!\n");
+        }
+        printf(
+        "\nDo you want to Coutinue? 1=Yes,2=No");
+        scanf("%d",&choice);
+        if(choice==2)
+        {
+            printf("\nThank you! Visit again\n");
+        }
+        printf("\nEnter the amount to withdraw:");
+        scanf("%d",&withdraw);
+
+        if (withdraw<=Balance)
+        {
+            Balance-=withdraw;
+            printf("\nTransaction successful!\n");
+            printf("\nRemaining Balance: %d\n", Balance);
+        }
+        else
+        {
+            printf("insufficient balance!\n");
+        }
+        printf(
+        "\nDo you want to Coutinue? 1=Yes,2=No");
+        scanf("%d",&choice);
+        if(choice==2)
+        {
+            printf("\nThank you! Visit again!!\n");
+            break;
+        }
+        else
+            continue;
+    }
+    return 0;
+}

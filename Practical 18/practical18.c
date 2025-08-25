58c6bf1b5e5e8bffd17a2f62884072ae64bf81a7
Rashi-Domadiya <rashidomadiya.ce@gmail.com>
2025-08-25 15:09:07 +0530

    Add files via upload

---
+++ Practical 18/practical18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int vertical, horizontal;
+    printf("Enter the size of table vertically: ");
+    scanf("%d", &vertical);
+    printf("Enter the size of table horizontally: ");
+    scanf("%d", &horizontal);
+    if (vertical <= 0 || horizontal <= 0) {
+        printf("Invalid input! Please enter positive integers only.\n");
+        return 1;
+    }
+    printf("\nMultiplication Table (%d x %d):\n\n", vertical, horizontal);
+    for (int i = 1; i <= vertical; i++) {
+        for (int j = 1; j <= horizontal; j++) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
